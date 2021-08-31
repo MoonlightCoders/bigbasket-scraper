87a4d1e10f069fdcd522a1db2a5149af02dfca26
Gowtham <gowtham.p@in.reportbee.com>
2021-08-31 21:01:57 +0530

    Changes

--- Script.py
+++ Script.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup as bs
 import requests
 import shutil
@@ -9,11 +10,11 @@ import re
 
 def start_driver(headless=True):
     if not headless:
-        return webdriver.Chrome('./chromedriver')
+        return webdriver.Chrome(ChromeDriverManager().install())
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
-    return webdriver.Chrome('./chromedriver', options=chrome_options)
+    return webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 
 
 def save_image(image_link, save_dir):
@@ -24,26 +25,54 @@ def save_image(image_link, save_dir):
         shutil.copyfileobj(image_raw.raw, out_file)
 
 
-def get_product_data(product, raw_data_file):
-    img = product.find("img")['src']
-    image_small = img.replace('/media/uploads/p/mm/', '/media/uploads/p/s/')
-    image_large = img.replace('/media/uploads/p/s/', '/media/uploads/p/l/'). \
-        replace('/media/uploads/p/mm/', '/media/uploads/p/l/')
+def get_product_data(driver1, product, raw_data_file):
+    link = product.find("div", {"qa": "product_name"}).find("a").attrs['href']
+    full_link = 'https://www.bigbasket.com' + link
+    print(full_link)
 
-    Brand = product.find("div", {"qa": "product_name"}).find("h6").text
-    Product = product.find("div", {"qa": "product_name"}).find("a").text
-    Quantity = product.find("span", {"data-bind": "label"}).text
-    Price = product.find("span", {"class": "discnt-price"}).text
+    driver1.get(full_link)
+
+    product_data = {}
+    product_data['product_code'] = link.split('/')[2]
+    product_data['product_link'] = full_link
+
+    product_data['brand'] = driver1.find_element_by_css_selector("a[context='brandlabel']").text
+    product_data['product_price'] = (driver1.find_element_by_css_selector("td[data-qa='productPrice']").text).replace('Rs ', '')
+
+    bread_crumbs = driver1.find_elements_by_class_name('_3WUR_')
+    product_data['product_name'] = bread_crumbs[4].text
+    product_data['master_category'] = bread_crumbs[1].text
+    product_data['sub_category'] = bread_crumbs[2].text
+    product_data['micro_category'] = bread_crumbs[3].text
+
+    additions_infos = driver1.find_elements_by_class_name('_3ezVU')
+    for index in range(len(additions_infos)) :
+        additions_info = additions_infos[index]
+        title = additions_info.find_element_by_class_name('_3LyVz').text
+        description = additions_info.find_element_by_class_name('_26MFu').text
+        product_data[title] = description
+
+        if title == 'Other Product Info' :
+            product_data["EAN Code"] = description.split('EAN Code: ')[1].split('\n')[0]
+            product_data[title] = product_data[title].split('For Queries/Feedback/')[0]
+
+    images = driver1.find_elements_by_class_name('_3oKVV')
+    for index in range(len(images)) :
+        product_data['image_'+ str(index+1)] = images[index].get_attribute("src")
+
+
+    # img = product.find("img")['src']
+    # image_small = img.replace('/media/uploads/p/mm/', '/media/uploads/p/s/')
+    # image_large = img.replace('/media/uploads/p/s/', '/media/uploads/p/l/'). \
+    #     replace('/media/uploads/p/mm/', '/media/uploads/p/l/')
+
+    # Brand = product.find("div", {"qa": "product_name"}).find("h6").text
+    # Product = product.find("div", {"qa": "product_name"}).find("a").text
+    # Quantity = product.find("span", {"data-bind": "label"}).text
+    # Price = product.find("span", {"class": "discnt-price"}).text
 
     with open(raw_data_file, "a") as f:
-        data = json.dumps({
-            'Brand': Brand,
-            'Product': Product,
-            'Quantity': Quantity,
-            'Price': Price,
-            'image_small': image_small,
-            'image_large': image_large,
-        })
+        data = json.dumps(product_data)
         f.write(data + "\n")
 
     # try:
@@ -110,7 +139,7 @@ if __name__ == "__main__":
             os.makedirs(dl_img)
 
         for product in products:
-            get_product_data(product, raw_data_file)
+            get_product_data(driver, product, raw_data_file)
 
         print("Downloaded all data from: ".format(url))
 
